f16328e6c43549150117a83a46233e5bd8ce0816
marialuizapsilva <maria.luiza03082006@gmail.com>
2024-04-23 16:38:24 -0300

    Create Contato1.cs
    criada a classe Contato.cs, criado os metodos

---
+++ ListaDeContatosMalu/Contato1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaDeContatosMalu
+{
+    internal class Contato1
+    {
+       //declaração de variaveis
+        private string nome;
+        private string sobrenome;
+        private string telefone;
+
+        //propriedades
+        //get = leitura
+        //set = definiçao
+        //é possível ser somente leitura, ou somente definição
+        public string Nome
+        {
+            get
+            {
+                return nome;
+            }
+            set
+            {
+                nome = value;
+            }
+        }
+        public string Sobrenome
+        {
+
+           get 
+           { 
+            return sobrenome;   
+           }
+           set
+           {
+            sobrenome = value;
+           }
+        }
+        public string Telefone
+        {
+
+            get
+            {
+                return telefone;
+            }
+            set
+            {
+                if (value.Length == 13)
+                {
+                    telefone = value;
+                }
+                else
+                {
+                    telefone = " (00) 00000-0000";
+                }
+            }
+
+            public Contato(string nome, string sobrenome, string sobrenome);
+        
+        
+        }
+        
+    }
+}
